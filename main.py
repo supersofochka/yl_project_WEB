a13bb185d826180fc8951e791f090eee43373c79
Sofiia Salikhova <sofiia.salikhova@yandex.ru>
2021-04-22 14:48:58 +0300

    third version of main page

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ def main_page():
     return render_template('main_page.html', title='Домашняя страница')
 
 
+@app.route('/enter')
+def enter():
+    return 'Вход'
+
+
+@app.route('/registration')
+def registration():
+    return 'Регистрация'
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
