5c8be702653a6507f976718d0e972839b818f81f
Sofiia Salikhova <sofiia.salikhova@yandex.ru>
2021-04-22 15:44:27 +0300

    first version of db

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, request, render_template
+from data import db_session
 
 app = Flask(__name__)
 
@@ -10,13 +11,14 @@ def main_page():
 
 @app.route('/enter')
 def enter():
-    return 'Вход'
+    return render_template('enter.html', title='Страница входа')
 
 
 @app.route('/registration')
 def registration():
-    return 'Регистрация'
+    return render_template('registration.html', title='Страница регистрации')
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/users.db")
     app.run(port=8080, host='127.0.0.1')
