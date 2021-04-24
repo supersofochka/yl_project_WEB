a80b3bab8ea6318c7c808bff819e4e9a24b21094
Sofiia Salikhova <sofiia.salikhova@yandex.ru>
2021-04-24 11:11:35 +0300

    heroku 2.0

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request
 from data import db_session
 from forms.user import RegisterForm, EnterForm
 from data.users import User
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -103,4 +104,5 @@ def result_order():
 
 if __name__ == '__main__':
     db_session.global_init("db/users.db")
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port, host='127.0.0.1')
