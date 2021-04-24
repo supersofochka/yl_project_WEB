3594761c964dc3a840931af27e9c9c4225991fc9
Sofiia Salikhova <sofiia.salikhova@yandex.ru>
2021-04-24 11:23:35 +0300

    heroku 4.0

--- main.py
+++ main.py
@@ -105,4 +105,4 @@ def result_order():
 if __name__ == '__main__':
     db_session.global_init("db/users.db")
     port = int(os.environ.get("PORT", 5000))
-    app.run(port=port, host='0.0.0.0')
+    app.run(port=port, host='127.0.0.1')
